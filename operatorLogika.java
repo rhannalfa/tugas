c682b66b6a8091dd6bd3acf20956d77e1e9e66a3
rhannalfa <137135461+rhannalfa@users.noreply.github.com>
2023-09-21 21:50:37 +0700

    Add files via upload

---
+++ operatorLogika.java
@@ -0,0 +1,16 @@
+package Pertemuan3;
+public class operatorLogika {
+    public static void main(String [] args){
+		int  A = 3; 
+        int  B = 6; 
+        int  C = 2; 
+        int  K = 5; 
+        int  L = 4; 
+        int  M = 3;
+		System.out.println(4 + 2 > A && B - 2 > 3 + 2 || B + 2 <= 6 + 2);
+        System.out.println(K + 5 < M || C * M < L && 2 * M - L > 0);
+        System.out.println(L + 5 < M || C * K < L && 2 * K - L > 0);
+        System.out.println(A * 4 <= 3 * M + B);
+        System.out.println(K + 10 > A && L - 2 > 4 * C);
+    }
+}
---
+++ phytagoras.java
@@ -0,0 +1,18 @@
+package Pertemuan3;
+
+import java.util.Scanner;
+public class phytagoras {
+    public static void main(String[]args) {
+        Scanner input = new Scanner(System.in);
+
+        
+            System.out.print("Masukan A = ");
+            int A = input.nextInt();
+            System.out.print("Masukan B = ");
+            int B = input.nextInt();
+            double C = ((A*A)+(B*B));
+            System.out.print("Mencari C = " + Math.sqrt(C));
+
+        
+    }
+}
