992ae31f2e67611dba3e39db16c2667d269607e3
rhannalfa <137135461+rhannalfa@users.noreply.github.com>
2023-09-27 14:24:31 +0700

    Update operatorLogika.java

--- operatorLogika.java
+++ operatorLogika.java
@@ -7,13 +7,17 @@ public class operatorLogika {
         int  L = 4; 
         int  M = 3;
         System.out.print("a. ");
-		System.out.println((4 + 2 > A) && (B - 2 > 3 + 2) || (B + 2 <= 6 + 2));
+	System.out.println((4 + 2 > A) && (B - 2 > 3 + 2) || (B + 2 <= 6 + 2));
+	    
         System.out.print("b. ");
         System.out.println((K + 5 < M) || (C * M < L) && (2 * M - L > 0));
+	    
         System.out.print("c. ");
         System.out.println((L + 5 < M) || (C * K < L) && (2 * K - L > 0));
+	    
         System.out.print("d. ");
         System.out.println(A * 4 <= 3 * M + B);
+	    
         System.out.print("e. ");
         System.out.println((K + 10 > A) && (L - 2 > 4 * C));
     }
