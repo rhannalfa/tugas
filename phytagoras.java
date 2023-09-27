5bd85473218fa52f6a869b4f2b4d6c92f67ad247
rhannalfa <137135461+rhannalfa@users.noreply.github.com>
2023-09-27 22:37:31 +0700

    Update phytagoras.java

--- phytagoras.java
+++ phytagoras.java
@@ -2,12 +2,42 @@ import java.util.Scanner;
 public class phytagoras {
     public static void main(String[]args) {
         Scanner input = new Scanner(System.in);
-        
-            System.out.print("Masukan A = ");
+
+        System.out.println("=== Rumus Phytagoras Segitiga Siku-Siku ===");
+        System.out.println("1. Mencari Sisi Miring");
+        System.out.println("2. Mencari Sisi Alas");
+        System.out.println("3. Mencari Sisi Tinggi");
+
+        System.out.println("===========================================");
+        System.out.print("Pilihan = ");
+        int a = input.nextInt();
+        System.out.println("===========================================");
+        if(a == 1){
+            System.out.print("Masukan Sisi Alas = ");
             int A = input.nextInt();
-            System.out.print("Masukan B = ");
+            System.out.print("Masukan Sisi Tinggi = ");
             int B = input.nextInt();
-            double C = ((A*A)+(B*B));
-            System.out.print("Mencari C = " + Math.sqrt(C));
+            int C = ((A*A)+(B*B));
+            System.out.println("Sisi Miring = " + Math.sqrt(C));
+
+        }else if(a == 2){
+            System.out.print("Masukan Sisi Tinggi = ");
+            int B = input.nextInt();
+            System.out.print("Masukan Sisi Miring = ");
+            int C = input.nextInt();
+            int A = ((B*B)-(C*C));
+            System.out.println("Sisi Alas = " + Math.sqrt(Math.sqrt(A*A)));
+
+        }else if(a == 3){
+            System.out.print("Masukan Sisi Alas= ");
+            int A = input.nextInt();
+            System.out.print("Masukan Sisi Miring = ");
+            int C = input.nextInt();
+            int B = (A*A-C*C);
+            System.out.println("Sisi Tinggi = " + Math.sqrt(Math.sqrt(B*B)));
+        }else{
+            System.out.print("Pilihan anda salah!!");
+        }
+        System.out.println("===========================================");
     }
 }
